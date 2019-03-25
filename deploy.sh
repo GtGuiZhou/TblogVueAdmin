829adf3a0c978fc31e5f90213a1efe98b93277b2
gtguizhou <gtguizhou@qq.com>
2019-03-25 11:54:28 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cnpm i
+npm i # d2-admin需要用nrm配置npm的源，要不然会出现core-js无法加载的问题
 npx vue-cli-service build --mode production --dest /www/wwwroot/TBlog/public/static/admin --target app --dashboard
 # 这种方法也可以
 # ./node_modules/.bin/vue-cli-service build
